d11eb51f2a7216550a90d8f5d612d1a38ae16c2d
Kannika Phadoungxath <kannika.phad@gmail.com>
2021-06-07 10:01:23 -0400

    New domain

--- neekphad_dotcom/settings.py
+++ neekphad_dotcom/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['kphad.pythonanywhere.com']
+ALLOWED_HOSTS = ['www.neekphad.com']
 
 
 # Application definition
